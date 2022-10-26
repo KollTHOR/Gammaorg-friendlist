32d6582436c27994b2d9190eb887ce3310cd7ce1
kollthor <abonyiaron1999@gmail.com>
2022-10-26 21:15:38 +0200

    angular base files

--- Frontend/Friend-list/src/app/app-routing.module.ts
+++ Frontend/Friend-list/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { addFriendsPageComponent } from './add-friends-page/add-friends-page.component';
+import { ListAllFriendsPageComponent } from './list-all-friends-page/list-all-friends-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:addFriendsPageComponent},
+  //{path: '/list', component:ListAllFriendsPageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/Friend-list/src/app/app.module.ts
+++ Frontend/Friend-list/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { addFriendsPageComponent } from './add-friends-page/add-friends-page.component';
+import { FriendTileComponent } from './friend-tile/friend-tile.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ListAllFriendsPageComponent } from './list-all-friends-page/list-all-friends-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    addFriendsPageComponent,
+    FriendTileComponent,
+    ListAllFriendsPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
